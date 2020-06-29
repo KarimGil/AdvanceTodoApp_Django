081b92fc91656d9d781c373b0841a20a4ecdda28
KarimGil <karimg87@hotmail.com>
2020-06-29 11:55:29 -0700

    Almost finishing up the functionality

--- todo/urls.py
+++ todo/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('signup',views.signupuser, name='signupuser'),
     path('logout',views.logoutuser, name='logoutuser'),
     path('login',views.loginuser, name='loginuser'),
-
+    path('create', views.createtodos, name = 'createtodos'),
     path('currenttodos', views.currenttodos, name = 'currenttodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name = 'viewtodo'),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
+from .models import Todo
 
 # Create your views here.
 def index(request):
@@ -47,8 +49,35 @@ def logoutuser(request):
         logout(request)
         return redirect('/')
         
+def createtodos(request):
 
-    
+    if request.method == 'POST':
+        
+        form = TodoForm(request.POST)
+        newtodo = form.save(commit=False)
+        newtodo.user = request.user
+        newtodo.save()
+        return redirect('currenttodos')
+        
+    else:
+        return render(request, 'createtodos.html', {'form':TodoForm()})
+        
+
 
 def currenttodos(request):
-    return render (request,'currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+
+    return render(request, 'currenttodos.html', {'todos':todos})
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'viewtodo.html', {'todo':todo, 'form':form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'viewtodo.html', {'todo':todo, 'form':form, 'error':'Bad info'})
